7951d48fc90cc9c7edeab55593f9d8ba869da4d1
Bill Denney <wdenney@humanpredictions.com>
2023-04-29 22:24:55 -0400

    Ensure that compartment initialization works with targets

--- tests/testthat/test-tar_nlmixr.R
+++ tests/testthat/test-tar_nlmixr.R
@@ -70,3 +70,47 @@ targets::tar_test("tar_nlmixr execution", {
     inherits(tar_read(pheno_model), "nlmixr2FitCore")
   )
 })
+
+# targets::tar_test() runs the test code inside a temporary directory
+# to avoid accidentally writing to the user's file space.
+targets::tar_test("tar_nlmixr handling with initial conditions central(initial)", {
+  targets::tar_script({
+    pheno <- function() {
+      ini({
+        lcl <- log(0.008); label("Typical value of clearance")
+        lvc <-  log(0.6); label("Typical value of volume of distribution")
+        etalcl + etalvc ~ c(1,
+                            0.01, 1)
+        cpaddSd <- 0.1; label("residual variability")
+      })
+      model({
+        cl <- exp(lcl + etalcl)
+        vc <- exp(lvc + etalvc)
+        kel <- cl/vc
+        d/dt(central) <- -kel*central
+        cp <- central/vc
+        central(initial) <- 0
+        cp ~ add(cpaddSd)
+      })
+    }
+
+    nlmixr2targets::tar_nlmixr(
+      name=pheno_model,
+      object=pheno,
+      data=nlmixr2data::pheno_sd,
+      est="saem",
+      # Minimize time spent
+      control=nlmixr2est::saemControl(nBurn=1, nEm=1)
+    )
+  })
+  expect_equal(
+    targets::tar_outdated(callr_function = NULL),
+    c("pheno_model_tar_object_simple", "pheno_model_tar_data_simple", "pheno_model")
+  )
+  suppressWarnings(targets::tar_make(callr_function = NULL))
+  # A successful model estimation step should return an nlmixr2FitCore object
+  # (testing of model results is outside the scope of nlmixr2targets)
+  expect_true(
+    inherits(tar_read(pheno_model), "nlmixr2FitCore")
+  )
+})
