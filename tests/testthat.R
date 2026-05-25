821b7cec6c4bc3dd8cf0548076c7867bcf486435
Bill Denney <wdenney@humanpredictions.com>
2026-05-25 07:29:48 -0400

    Make tests run serially to prevent more than 2 cores being used on CRAN (which breaks CRAN policy)

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,4 +3,19 @@ library(testthat)
 library(targets)
 library(nlmixr2targets)
 
+# Remove to replace with on_cran if exported,
+# https://github.com/r-lib/testthat/issues/2336
+on_cran_local <- function () {
+    env <- Sys.getenv("NOT_CRAN")
+    if (identical(env, "")) {
+        !interactive()
+    }
+    else {
+        !isTRUE(as.logical(env))
+    }
+}
+
+if (on_cran_local()) {
+  rxode2::setRxThreads(1L)
+}
 test_check("nlmixr2targets")
